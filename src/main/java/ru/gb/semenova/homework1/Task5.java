d76bff9ade4ae0ae3e3da9192f207c5cb112e512
Alena <gaidenal5@gmail.com>
2022-04-18 17:57:28 +0300

    Task 5, create method compareNumbers.

---
+++ src/main/java/ru/gb/semenova/homework1/Task5.java
@@ -0,0 +1,16 @@
+package ru.gb.semenova.homework1;
+
+public class Task5 {
+    public static void  compareNumbers() {
+        int a = 3;
+        int b = 3;
+        if (a >= b){
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+}
