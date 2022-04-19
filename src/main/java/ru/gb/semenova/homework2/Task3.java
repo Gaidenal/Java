796319478c24f948e0e12c99d7b40f3298174f2a
Alena <gaidenal5@gmail.com>
2022-04-20 00:31:06 +0300

    Homework 2 Task 3.

---
+++ src/main/java/ru/gb/semenova/homework2/Task3.java
@@ -0,0 +1,10 @@
+package ru.gb.semenova.homework2;
+
+public class Task3 {
+    public static boolean positiveOrNegative(int a){
+       return (a < 0);
+    }
+    public static void main(String[] args) {
+      System.out.println(positiveOrNegative(-1));
+    }
+}
