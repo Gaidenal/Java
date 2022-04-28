c2d7d0c7165bb8cc81505c6ed708a1cc85c7e10a
Alena <gaidenal5@gmail.com>
2022-04-28 22:22:45 +0300

    Homework 4.

--- src/main/java/ru/gb/semenova/homework4/Task1.java
+++ src/main/java/ru/gb/semenova/homework4/Task1.java
@@ -6,8 +6,10 @@ public class Task1 {
     public static final int WIDTH = 10;
     public static final int HEIGHT = 10;
     public static final int MINES_COUNT = 20;
-
     public static final int MINE = 1000;
+    public static final int EMPTY = 0;
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main(String[] args) {
 
         play();
@@ -17,9 +19,6 @@ public class Task1 {
         boolean win = true;
         int[][] board = generateBoard();
         printBoard(board);
-    }
-
-    public static void printBoard(int[][] board) {
     }
 
     public static int[][] generateBoard() {
@@ -44,6 +43,9 @@ public class Task1 {
                 int mCount = 0;
                 for (int k = i - 1; k <= i + 1; k++) {
                     for (int l = j - 1; l <= j + 1; l++) {
+                        if (k < 0 || k >= HEIGHT || l < 0 || l >= WIDTH){
+                            continue;
+                        }
                         if (board[k][l] == MINE) {
                             mCount++;
                         }
@@ -54,5 +56,26 @@ public class Task1 {
         }
 
         return board;
+    }
+
+    public static void printBoard(int[][] board) {
+        System.out.println("    ");
+        for (char i = 'A'; i < 'A' + WIDTH; i++ ){
+            System.out.print("  " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < HEIGHT; i++){
+            System.out.printf("%3d", i);
+            for (int j = 0; j < WIDTH; j++){
+                if (board[i][j] == EMPTY){
+                    System.out.print(" .");
+                } else if (board[i][j] == MINE) {
+                    System.out.print(" *");
+                } else {
+                    System.out.printf("%2d", board[i][j]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
